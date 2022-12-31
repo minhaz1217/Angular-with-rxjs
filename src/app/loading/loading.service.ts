ed779af044c8a2b9ad3e5a944eaedc2accaa911b
Minhaz <minhaz1217@gmail.com>
2022-12-31 23:49:42 +0600

    loading observable added with ngif

--- src/app/loading/loading.service.ts
+++ src/app/loading/loading.service.ts
@@ -1,7 +1,19 @@
 import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
 
 
 @Injectable()
 export class LoadingService{
+    loading$ : Observable<boolean>;
     
+    showLoaderUntilCompleted<T>(obs$ :Observable<T>): Observable<T>{
+        return undefined;
+    }
+
+    loadingOn(){
+        
+    }
+    loadingOff(){
+
+    }
 }
