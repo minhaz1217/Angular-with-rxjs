bfabf4382784fd0808d6aedc933a64832dcb183d
Minhaz <minhaz1217@gmail.com>
2023-01-03 01:46:36 +0600

    change detection strategy changed to onpush, this only works on deteching changed in @Input or | async

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -1,24 +1,10 @@
-import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Course } from '../model/course';
-import {
-  debounceTime,
-  distinctUntilChanged,
-  startWith,
-  tap,
-  delay,
-  map,
-  concatMap,
-  switchMap,
-  withLatestFrom,
-  concatAll, shareReplay, catchError
-} from 'rxjs/operators';
-import { merge, fromEvent, Observable, concat, throwError, combineLatest } from 'rxjs';
+import { startWith, tap, map, } from 'rxjs/operators';
+import { Observable, combineLatest } from 'rxjs';
 import { Lesson } from '../model/lesson';
 import { CoursesService } from '../services/courses.service';
-
-
-
 
 interface CourseData {
   course: Course;
@@ -27,7 +13,8 @@ interface CourseData {
 @Component({
   selector: 'course',
   templateUrl: './course.component.html',
-  styleUrls: ['./course.component.css']
+  styleUrls: ['./course.component.css'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CourseComponent implements OnInit {
 
